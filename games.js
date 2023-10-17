e481358baec8a166d704d8876c708af02e2e3d29
Austin Curtis <austin.curtis.mail@gmail.com>
2023-10-16 22:04:23 -0500

    Game, lol

--- games.js
+++ games.js
@@ -1,10 +1,12 @@
-import { clamp } from './utils.js'
+import { clamp, testForAABB } from './utils.js'
 
 let app = initApp(window)
 document.body.appendChild(app.view)
 
 const [left, up, right, down] = setUpKeyboard()
-const [bunny, speedText, locationText] = setUpSprites()
+const [bunny, bunnySprite, speedText, pointsText, locationText] = setUpSprites()
+const star = getStar()
+let points = 0
 start()
 
 export function initApp(element) {
@@ -13,14 +15,33 @@ export function initApp(element) {
 
 export function start() {
   // Listen for animate update
+  drawStar()
   app.ticker.add((delta) => gameLoop(delta));
 }
 
 export function gameLoop(delta) {
   updateSpeed()
   updatePosition(delta)
+  checkCollision()
+  pointsText.text = `Points: ${points} ✨`
+  const [fontSize, _] = clamp(points / 2, 12, 200)
+  pointsText.TextStyle = {
+    fontSize: 200
+  }
   speedText.text = `Speed: (${xSpeed},${ySpeed})`
   locationText.text = `(${Math.floor(bunny.x)},${Math.floor(bunny.y)})`
+}
+
+function checkCollision() {
+  if (testForAABB(bunnySprite, star)) {
+    app.stage.removeChild(star)
+    points += 10
+    drawStar()
+    document.body.classList.add("apply-shake");
+    setTimeout(() => {
+      document.body.classList.remove("apply-shake");
+    }, 500)
+  }
 }
 
 export function reset() {
@@ -87,6 +108,7 @@ let ySpeed = 0
 export function setUpSprites() {
   const bunny = new PIXI.Container()
   const speedText = new PIXI.Text()
+  const pointsText = new PIXI.Text()
   const locationText = new PIXI.Text()
   // create a new Sprite from an image path
   app.stage.addChild(bunny);
@@ -104,8 +126,10 @@ export function setUpSprites() {
   bunny.addChild(locationText)
   
   app.stage.addChild(speedText)
+  pointsText.y = 50
+  app.stage.addChild(pointsText)
 
-  return [bunny, speedText, locationText]
+  return [bunny, bunnySprite, speedText, pointsText, locationText]
 }
 
 
@@ -146,4 +170,18 @@ function keyboard(keyCode) {
     window.addEventListener("keydown", key.downHandler.bind(key), false);
     window.addEventListener("keyup", key.upHandler.bind(key), false);
     return key;
+}
+
+function getStar() {
+  const star = new PIXI.Text("⭐️", new PIXI.TextStyle({
+    fontSize: 30,
+  }))
+  return star
+}
+
+function drawStar() {
+  star.x = Math.random() * app.screen.width;
+  star.y = Math.random() * app.screen.height;
+  star.tint = Math.random() * 0x808080;
+  app.stage.addChild(star);
 }
